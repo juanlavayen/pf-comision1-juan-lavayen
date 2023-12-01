aeec53994a241a3558be1e066d6c3bf77a013a87
Juan Lavayen <juanlavayen@hotmail.com>
2023-11-30 21:53:33 -0300

    ActualizaFront
    Cargadearchivos.

---
+++ frontend/src/pages/CommentPage.jsx
@@ -0,0 +1,102 @@
+import { useContext, useEffect, useRef, useState } from "react";
+import { useParams } from "react-router-dom";
+import { API_URL } from "../utils/consts";
+import { AuthContext } from "../providers/AuthProvider";
+import { HiOutlineTrash } from "react-icons/hi";
+import Navbar from "../components/Navbar";
+
+const CommentPage = () => {
+  const { postId } = useParams();
+
+  const [post, setPost] = useState(null);
+
+  const { auth } = useContext(AuthContext);
+  const formRef = useRef(null);
+
+  const getPost = () => {
+    fetch(`${API_URL}/post/${postId}`, {
+      headers: {
+        Authorization: auth.token,
+      },
+    })
+      .then((res) => {
+        if (res.status !== 200) return alert("Error getting post");
+
+        return res.json();
+      })
+      .then((data) => {
+        setPost(data);
+      });
+  };
+
+  useEffect(() => {
+    getPost();
+  }, [postId, auth]);
+
+  const handleDeleteComment = (commentId) => {
+    fetch(`${API_URL}/comments/${postId}/${commentId}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: auth.token,
+      },
+    }).then((res) => {
+      if (!res.ok) return alert("Error deleting comment");
+      getPost();
+    });
+  };
+
+  const handleCreateNewComment = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+
+    fetch(`${API_URL}/comments/${postId}`, {
+      method: "POST",
+      body: JSON.stringify({
+        name: formData.get("name"),
+        artist: formData.get("author"),
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: auth.token,
+      },
+    }).then((res) => {
+      if (!res.ok) return alert("Error creating new comment");
+      getPost();
+    });
+
+    formRef.current.reset();
+  };
+
+  if (!post) return <h1>Cargando...</h1>;
+
+  return (
+    <div>
+      <Navbar />
+      <h1>{post.title}</h1>
+      <form onSubmit={handleCreateNewComment} ref={formRef}>
+        <input type="text" name="name" placeholder="comment name" />
+        <input type="text" name="author" placeholder="author" />
+        <button>Create new Comment</button>
+      </form>
+      {post.comments.map((comment) => {
+        return (
+          <div key={comment.id} className="comment">
+            <h2>{comment.name}</h2>
+            <p>
+              <i>{comment.artist}</i> {comment.year}
+            </p>
+            <button
+              className="delete-button"
+              onClick={() => handleDeleteComment(comment._id)}
+            >
+              <HiOutlineTrash />
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default CommentPage;
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,42 @@
+// HomePage.js
+import styles from "../styles/Landing.module.css";
+import { Link } from "react-router-dom";
+
+function HomePage() {
+  return (
+    <div className={styles.container}>
+      <h1>Bienvenido a ExpTravel !!! </h1>
+      <p>
+        ¡Bienvenidos a ExpTravel, el fascinante rincón digital donde las experiencias de viaje cobran vida a través de relatos apasionantes! Sumérgete en un mundo diverso de aventuras contadas por viajeros de todos los rincones del planeta. Desde las majestuosas montañas hasta las playas más exóticas, nuestros narradores te transportarán a destinos lejanos con cada palabra. Únete a esta comunidad global de trotamundos, comparte tus propias odiseas y descubre la riqueza cultural que el mundo tiene para ofrecer. ExpTravel es tu pasaporte a la inspiración, la emoción y la conexión humana a través de las maravillas del viaje. ¡Prepárate para explorar el mundo desde la comodidad de tu pantalla y dejar que cada historia te lleve a una nueva y emocionante travesía! ¡Bienvenidos a bordo! 🌍✈️
+      </p>
+      
+      <Link to="/post">Busca tu nuevo destino !! </Link>
+      <br />
+      <hr />
+      <div className={styles.buttonContainer}>
+        <Link to="/register">Registro</Link>
+        <span className={styles.buttonSeparator}> | </span>
+        <Link to="/login">Login </Link>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
+
+
+/* import styles from "../styles/Landing.module.css";
+import { Link } from "react-router-dom";
+
+function HomePage() {
+  return (
+    <div className={styles.container}>
+      <h1>Bienvenido a ExpTravel !!! </h1>
+      <p>
+      ¡Bienvenidos a ExpTravel, el fascinante rincón digital donde las experiencias de viaje cobran vida a través de relatos apasionantes! Sumérgete en un mundo diverso de aventuras contadas por viajeros de todos los rincones del planeta. Desde las majestuosas montañas hasta las playas más exóticas, nuestros narradores te transportarán a destinos lejanos con cada palabra. Únete a esta comunidad global de trotamundos, comparte tus propias odiseas y descubre la riqueza cultural que el mundo tiene para ofrecer. ExpTravel es tu pasaporte a la inspiración, la emoción y la conexión humana a través de las maravillas del viaje. ¡Prepárate para explorar el mundo desde la comodidad de tu pantalla y dejar que cada historia te lleve a una nueva y emocionante travesía! ¡Bienvenidos a bordo! 🌍✈️
+      </p>
+      <Link to="/playlist">Go to Playlist's</Link>
+    </div>
+  );
+}
+export default HomePage; */
