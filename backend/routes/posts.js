32418776436ba7758028424f618964dd47ab02fd
Juan Lavayen <juanlavayen@hotmail.com>
2023-11-30 21:51:21 -0300

    ErrorCrash
    Limpieza de proyecto, error de modulos, falla de archivos.

--- backend/routes/posts.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const postsController = require("../controllers/posts");
-
-const router = express.Router();
-
-router.get("/", postsController.getPosts);
-
-module.exports = router;
--- backend/routes/users.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const usersController = require("../controllers/users");
-
-const router = express.Router();
-
-router.post("/register", usersController.register);
-
-module.exports = router;
