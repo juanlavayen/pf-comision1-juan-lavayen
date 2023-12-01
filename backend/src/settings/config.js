04ba11f9dce5dc5f06decdb83d1968406d1643da
Juan Lavayen <juanlavayen@hotmail.com>
2023-11-30 22:03:59 -0300

    EliminacionArchivosObsoletos
    Se eliminan archivos obsoletos de ejemplo

--- backend/src/settings/config.js
+++ backend/src/settings/config.js
@@ -4,5 +4,5 @@ export const config = {
   port: process.env.PORT || 3000,
   mongo: process.env.MONGO_URI || 'mongodb://localhost:27017/express-mongo',
   jwt_secret: process.env.JWT_SECRET || 'secret',
-  database: process.env.DATABASE_NAME || 'playlist-app',
+  database: process.env.DATABASE_NAME || 'expviaje',
 };
